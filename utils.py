bfb3505ab47f5ce30e0e92a2459e2638627ef0ab
Moritz Gärtner <moritzgaertner516@gmail.com>
2021-11-10 23:14:32 +0100

    new intersection + out of bounds calculation

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from random import randint
+import math
 
-from consts import CUBE_SIZE
+from consts import CIRCLE_SIZE, OFFSET, SQUARE_SIZE, SQUAREROOT_TWO, SQUARES_PER_PLAYER
 
 def random_color() -> str:
     return "#%02x%02x%02x" % (randint(0, 255), randint(0, 255), randint(0, 255))
@@ -17,16 +18,29 @@ def get_color_int(color: str) -> int:
         return 3
     return -1
 
-    if (distance_x > (rectangle_size/2 + ball_radius)):
+
+def intersects(ball_position: tuple[float, float], rectangle_position: tuple[int, int], ball_radius: int = CIRCLE_SIZE, rectangle_size: int = SQUARE_SIZE) -> bool:
+    r_max = (SQUAREROOT_TWO/2)*rectangle_size
+    dist = math.hypot(ball_position[0] - rectangle_position[0], ball_position[1] - rectangle_position[1])
+
+    if dist > ball_radius + r_max:
         return False
-    if (distance_y > (rectangle_size/2 + ball_radius)):
-        return False
-
-    if (distance_x <= (rectangle_size/2)):
-        return True
-    if (distance_y <= (rectangle_size/2)):
+    
+    if dist < ball_radius + r_max:
         return True
 
-    cornerDistance_sq = (distance_x - rectangle_size/2)**2 + (distance_y - rectangle_size/2)**2
+    dist_x = abs(ball_position[0] - rectangle_position[0])
+    dist_y = abs(ball_position[1] - rectangle_position[1])
+    if dist_x == ball_radius + rectangle_size/2 or dist_y == ball_radius + rectangle_size/2:
+        return True
+    return False
 
-    return (cornerDistance_sq <= (ball_radius**2))
+
+def ball_out_of_bounds(ball) -> bool:
+    if OFFSET > ball.center[0] or OFFSET + 2*SQUARES_PER_PLAYER*SQUARE_SIZE < ball.center[0]:
+        return True
+    
+    if OFFSET > ball.center[1] or OFFSET + 2*SQUARES_PER_PLAYER*SQUARE_SIZE < ball.center[1]:
+        return True
+    
+    return False
