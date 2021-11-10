83b15443a0fb78a0c59ad81189cd79521864ce02
Moritz Gärtner <moritzgaertner516@gmail.com>
2021-11-10 23:12:32 +0100

    rename ball position to center + CIRCLE_SIZE as constant

--- ball.py
+++ ball.py
@@ -2,34 +2,42 @@ from typing import Any
 from tkinter import Tk, Canvas, mainloop
 
 import numpy as np
+from consts import CIRCLE_SIZE
+from utils import get_color_int
 
 class Ball:
-    position: tuple[float, float]
+    
+    center: tuple[float, float]
     direction: tuple[float, float]
     radius: int
 
     color: str
+    color_int: int
 
     canvas: Canvas
     draw_obj: Any
 
-    def __init__(self, start_position: tuple[float, float], direction: tuple[float, float], color: str, canvas: Canvas, radius: int = 5) -> None:
+    def __init__(self, center: tuple[float, float], direction: tuple[float, float], color: str, canvas: Canvas, radius: int = CIRCLE_SIZE) -> None:
         self.direction = direction
-        self.position = start_position
+        self.center = center
         self.radius = radius
 
         self.color = color
+        self.color_int = get_color_int(self.color)
 
         self.canvas = canvas
-        self.draw_obj = self.canvas.create_oval(self.position[0]-self.radius, self.position[1]-self.radius, self.position[0]+self.radius, self.position[1]+self.radius, fill=self.color)
+        self.draw_obj = self.canvas.create_oval(self.center[0]-self.radius, self.center[1]-self.radius, self.center[0]+self.radius, self.center[1]+self.radius, fill=self.color)
 
 
     def update(self) -> None:
         self.update_position()
-        self.canvas.coords(self.draw_obj, self.position[0]-self.radius, self.position[1]-self.radius, self.position[0]+self.radius, self.position[1]+self.radius)
-
+        self.redraw()
+        
     def update_position(self) -> None:
-        self.position += self.direction / np.linalg.norm(self.direction) * 2
+        self.center += self.direction / np.linalg.norm(self.direction) * 2
+    def redraw(self) -> None:
+        self.canvas.coords(self.draw_obj, self.center[0]-self.radius, self.center[1]-self.radius, self.center[0]+self.radius, self.center[1]+self.radius)
+
     
     def __del__(self) -> None:
         self.canvas.delete(self.draw_obj)
